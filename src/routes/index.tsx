4a9987be45331c01f938bc5e57ecf2145012c328
Gyujae <wjdrbwo1206@naver.com>
2022-07-19 16:49:31 +0900

    first commit

---
+++ src/routes/index.tsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div className='flex min-h-screen min-w-min items-center justify-center bg-rose-400 font-semibold text-sky-400'>
+      Hello World
+    </div>
+  )
+}
+
+export default App
---
+++ src/styles/index.js
@@ -0,0 +1,5 @@
+import cx from 'classnames'
+import styles from './index.css'
+
+export default styles
+export { cx }
